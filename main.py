fb635b58cc9abc54463d670b6f1134492ed9c729
JungJik Lee <jungjik.lee@samsung.com>
2025-10-09 22:05:02 +0900

    2

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pyjokes
 from pyscript.web import page
 from js import console
 
-def get_joke(event):
+def get_joke(event, test_data):
     page["div#jokes"].innerHTML = f"{pyjokes.get_joke()} 🥁"
     console.log(event)
+    console.log(test_data)
 
