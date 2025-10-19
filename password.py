8412748480194f0615c2a476d050b551c149d15d
JungJik Lee <jungjik.lee@samsung.com>
2025-10-19 10:27:48 +0900

    test

--- password.py
+++ password.py
@@ -4,7 +4,9 @@ from pyscript import document
 
 @when("click", "#call_python")
 def test(event):
+    __call_python = document.querySelector("#call_python")
+    window.console.log(__call_python.value)
 
-    my_element = document.querySelector("#call_python")
-    window.console.log(my_element.value)
-    
+if __name__=="__main__":
+    print("run on console")
+        
