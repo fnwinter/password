1e1c8052745ad911f97516dcccfd4a32e2909678
JungJik Lee <fnwinter@gmail.com>
2025-10-19 22:55:26 +0900

    add copy clipboard

--- password.py
+++ password.py
@@ -1,28 +1,25 @@
+import json
+import base64
+import pyodide_js
+pyodide_js.loadPackage('cryptography')
+
 from pyscript import window
 from pyscript import when
 from pyscript import document
-import json
-import base64
-
-import pyodide_js
-await pyodide_js.loadPackage('cryptography')
 
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-
 @when("click", "#call_python")
 def __call_python_from_js(event):
     __call_python = document.querySelector("#call_python")
-    __value = __call_python.target.getAttribute("data-value")
-
+    __value = json.loads(event.target.getAttribute("data-value"))
+    
     __function_name = __value[0]
     __param1 = __value[1]
     __param2 = __value[2]
     __param3 = __value[3]
-    print(__function_name)
-    print(__param1)
     if __function_name == "generateUrl":
         generateUrl(__param1, __param2)
 
@@ -53,7 +50,7 @@ def generateUrl(data, master_password):
         encrypted_b64 = base64.urlsafe_b64encode(encrypted_data).decode('utf-8')
         
         # Generate URL with encrypted data
-        url = f"https://password-manager.com/data?encrypted={encrypted_b64}"
+        url = f"https://password-manager.com/?d={encrypted_b64}"
         
         # Update the fullUrlInput with the generated URL
         full_url_input = document.getElementById('fullUrlInput')
