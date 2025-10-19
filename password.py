59d3f698425d55a04cf811b76aee007fd7f06b2a
JungJik Lee <jungjik.lee@samsung.com>
2025-10-19 17:35:29 +0900

    fix python call

--- password.py
+++ password.py
@@ -3,9 +3,15 @@ from pyscript import when
 from pyscript import document
 
 @when("click", "#call_python")
-def test(event):
+def __call_python_from_js(event):
     __call_python = document.querySelector("#call_python")
-    window.console.log(__call_python.value)
+    __value = event.target.getAttribute("data-value")
+    __function_name = __value[0]
+    __param1 = __value[1]
+    __param2 = __value[2]
+    __param3 = __value[3]
+
+    print(__value)
 
 if __name__=="__main__":
     print("run on console")
